c2f127ddb181acfc6a8f7d8f8ae16ed27ffb7271
Philipp Klein <Phil@SilverBook.lan>
2020-08-17 19:50:06 +0200

    Displaying repos and current branch works

--- GitHelper/gitHelper.py
+++ GitHelper/gitHelper.py
@@ -1,6 +1,14 @@
 import os
 import sys
 import tkinter as tk
+from pygit2 import Repository
+
+
+class Theme:
+    def __init__(self):
+        self.fgMasterBranch = "blue"
+        self.fgDevelopBranch = "green"
+        self.fgOtherBranch = "orange"
 
 
 class Application(tk.Frame):
@@ -8,10 +16,15 @@ class Application(tk.Frame):
         super().__init__(root)
         self.basePath = basePath
         self.root = root
+
+        self.theme = Theme()
+
         self.setupWindow()
         self.pack()
         self.createElements()
         self.discoverRepos()
+        self.updateCurrentBranches()
+        self.updateRepoList()
 
     def setupWindow(self):
         w = self.root.winfo_screenwidth()
@@ -21,6 +34,10 @@ class Application(tk.Frame):
         self.root.attributes('-topmost', True)
         self.root.after_idle(root.attributes, '-topmost', False)
         self.root.title("Git Helper")
+
+    def clearFrame(self, frame: tk.Frame):
+        for widget in frame.winfo_children():
+            widget.destroy()
 
     def createElements(self):
         self.toolBar = tk.Frame(self)
@@ -34,24 +51,48 @@ class Application(tk.Frame):
             self.toolBar, text="pull all", command=self.pullAll)
         self.pullAllButton.pack(side="left")
 
-        self.repoList = tk.Frame(self.root, bg="yellow")
+        self.repoList = tk.Frame(self.root)
         self.repoList.pack(fill="both", expand=True)
 
     def discoverRepos(self):
         print("Discovering repositories...")
 
+        # Collect data on the repositories in the given base directory
         self.gitRepos = []
         for dir in os.listdir(self.basePath):
             sys.stdout.write(f"   Analyzing '{dir}'")
             candidate = os.path.join(self.basePath, dir)
             if ".git" in os.listdir(candidate):
                 print(" is a git repo.")
-                self.gitRepos.append({"name": dir, "path": candidate})
+                self.gitRepos.append(
+                    {"name": dir, "path": candidate, "branch": "---"})
             else:
                 print(" is not a git repo.")
 
+    def updateCurrentBranches(self):
+        for repo in self.gitRepos:
+            repo["branch"] = Repository(repo["path"]).head.shorthand
+
+    def updateRepoList(self):
+        # Display the repositories in the repo list
+        self.clearFrame(self.repoList)
+        row = 0
+        for repo in self.gitRepos:
+            nameLabel = tk.Label(self.repoList, text=repo["name"])
+            nameLabel.grid(row=row, column=0, sticky="W")
+
+            branchLabel = tk.Label(
+                self.repoList, text=repo["branch"], fg=self.theme.fgOtherBranch)
+            if repo["branch"] == "master":
+                branchLabel.configure(fg=self.theme.fgMasterBranch)
+            if repo["branch"] == "develop":
+                branchLabel.configure(fg=self.theme.fgDevelopBranch)
+            branchLabel.grid(row=row, column=1, sticky="W")
+            row += 1
+
     def showBranches(self):
         print("Showing branches")
+        self.updateRepoList()
 
     def pullAll(self):
         print("Pulling all repos")
@@ -67,4 +108,4 @@ if __name__ == "__main__":
 
     root = tk.Tk()
     app = Application(basePath=basePath, root=root)
-    # app.mainloop()
+    app.mainloop()
