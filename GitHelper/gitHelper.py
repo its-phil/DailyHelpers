87e3ec00933b5733beb1640aa6fd6f1673ae599e
Philipp Klein <Phil@SilverBook.lan>
2020-08-17 19:13:25 +0200

    Added application skeleton

---
+++ GitHelper/gitHelper.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+import sys
+
+
+class Application(tk.Frame):
+    def __init__(self, basePath: str, root: tk.Tk):
+        super().__init__(root)
+        self.basePath = basePath
+        self.root = root
+        self.setupWindow()
+        self.pack()
+        self.createElements()
+        self.discoverRepos()
+
+    def setupWindow(self):
+        w = self.root.winfo_screenwidth()
+        h = self.root.winfo_screenheight()
+        self.root.geometry("%dx%d+0+0" % (w, h))
+        self.root.lift()
+        self.root.attributes('-topmost', True)
+        self.root.after_idle(root.attributes, '-topmost', False)
+        self.root.title("Git Helper")
+
+    def createElements(self):
+        self.toolBar = tk.Frame(self)
+        self.toolBar.pack(side="top", fill="x", expand=True)
+
+        self.showBranchesButton = tk.Button(
+            self.toolBar, text="show branches", command=self.showBranches)
+        self.showBranchesButton.pack(side="left")
+
+        self.pullAllButton = tk.Button(
+            self.toolBar, text="pull all", command=self.pullAll)
+        self.pullAllButton.pack(side="left")
+
+        self.repoList = tk.Frame(self.root, bg="yellow")
+        self.repoList.pack(fill="both", expand=True)
+
+    def discoverRepos(self):
+        print("Discovering repositories")
+
+    def showBranches(self):
+        print("Showing branches")
+
+    def pullAll(self):
+        print("Pulling all repos")
+
+
+if __name__ == "__main__":
+    # Check the command line arguments
+    if(len(sys.argv) < 2):
+        raise Exception("Please provide the base path as the first argument")
+
+    basePath = sys.argv[1]
+    print(f"Opening application for basepath '{basePath}'...")
+
+    root = tk.Tk()
+    app = Application(basePath=basePath, root=root)
+    app.mainloop()
